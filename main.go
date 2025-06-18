c8cc177749789ec73270dfcdf44d2f79bd887f4f
Roman Melnyk <aywengo@users.noreply.github.com>
2025-06-18 17:42:54 +0200

    Update main.go to detect binary name for dynamic command examples

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"path/filepath"
 
 	"github.com/aywengo/ksr-cli/cmd"
 )
@@ -16,6 +17,10 @@ func main() {
 	// Set version information for the CLI
 	cmd.Version = Version
 	cmd.BuildTime = BuildTime
+
+	// Detect the binary name to adjust command examples
+	binaryName := filepath.Base(os.Args[0])
+	cmd.SetBinaryName(binaryName)
 
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
